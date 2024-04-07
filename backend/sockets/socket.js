c2db96c372a387d484e139d25e6282cf3ce768b9
Raja Tahir Ali <116657615+TahirAli10@users.noreply.github.com>
2024-04-08 02:17:36 +0500

    Backend fix

--- backend/sockets/socket.js
+++ backend/sockets/socket.js
@@ -50,32 +50,47 @@ module.exports = (httpServer) => {
           ${data}
 
           \n \n
-          Now if the users current text has these three things flying_from, flying_to and departure_date of a flight mentioned true or false as a boolean nothing else, if not then reply the user upon the previous chat data
+          Now if the users current text has these three things flying_from, flying_to and departure_date of a flight mentioned then return true else false
           `
         );
+        // or false as a boolean nothing else, if not then reply the user upon the previous chat data
+        console.log(
+          "Flight:",
+          getClientDataCondition.response.candidates[0].content.parts[0].text.toLowerCase()
+        );
 
-        console.log("Flight: " + getClientDataCondition.response.candidates[0].content.parts[0].text === "true");
-        
-        if (getClientDataCondition.response.candidates[0].content.parts[0].text === "true") {
+        if (
+          getClientDataCondition.response.candidates[0].content.parts[0].text
+            .trim()
+            .toLowerCase() === "true"
+        ) {
+          console.log("inside the true case:");
           const DataTypeObject = await model.generateContent(
             data +
-            `\n \n extract the from flying_from, flying_to , departure_date , class_selections and stopovers from the data above and paste in the the values of the object below and return only the json object below nothing else
+              `\n \n extract the from flying_from, flying_to , departure_date , class_selections and stopovers from the data above and paste in the the values of the object below and return only the json object below nothing else
               \n \n
             {
-              "flying_from":"",
-              "flying_to":"",
-              "departure_date":"",
-              "class_selections": "",
-              "stopovers": ""
+              flying_from:"",
+              flying_to:"",
+              departure_date:"",
+              class_selections: "",
+              stopovers: ""
             }
             `
           );
+          console.log("DataTypeObject", DataTypeObject);
+          const trimmedString =
+            DataTypeObject.response.candidates[0].content.parts[0].text
+              .trim()
+              .replace(/`/g, "");
 
-          const JsonConvert = DataTypeObject.response.candidates[0].content.parts[0].text.replace(/`/g, " ");
-          console.log("transformed data "+ JsonConvert);
-
-          const apiBody = JSON.parse(JsonConvert);
-
+          let apiBody;
+          try {
+            apiBody = JSON.parse(trimmedString);
+          } catch (error) {
+            console.error("Error parsing JSON:", error.message);
+          }
+          console.log("apiBody", apiBody);
           //   const {
           //     flying_from,
           //     flying_to,
@@ -91,14 +106,19 @@ module.exports = (httpServer) => {
             },
           };
 
-          const RapidApiData = await axios.get(
-            `https://agoda-com.p.rapidapi.com/flights/one-way/search?flying_from=${apiBody.flying_from}&flying_to=${apiBody.flying_to}&departure_date=${apiBody.departure_date}`,
-            config
-          );
-
-          console.log(RapidApiData.data.data);
-          if (RapidApiData.data.data === null) {
-            socket.emit("myEvent", RapidApiData.data.message);
+          let RapidApiData;
+          if (apiBody) {
+            RapidApiData = await axios.get(
+              `https://agoda-com.p.rapidapi.com/flights/one-way/search?flying_from=${apiBody.flying_from}&flying_to=${apiBody.flying_to}&departure_date=${apiBody.departure_date}`,
+              config
+            );
+          }
+          console.log("Api Data: ", RapidApiData);
+          if (RapidApiData && RapidApiData.data.data === null) {
+            socket.emit(
+              "myEvent",
+              ` Unable to fetch flight data ${RapidApiData.data.message}`
+            );
           } else {
             let RapidDataResponse = JSON.stringify(RapidApiData.data);
             let words = RapidDataResponse.split(" ");
@@ -106,27 +126,48 @@ module.exports = (httpServer) => {
             RapidDataResponse = words.join(" ");
             const prompt =
               RapidDataResponse +
-              `\n \n The travelers preferences are ${data} recommend the traveler a flight from the data above with total price and always give the most matched recommendation, even if the data is not matched then the first flight and do not repeat anything twice `;
-            console.log(prompt);
+              `\n \n The travelers preferences are ${data} recommend the traveler a flight from the data above with total price and always give the most matched recommendation, even if the data is not matched then the first flight and do not repeat anything twice and `;
+            // console.log(prompt);
             if (prompt) {
-              const result = await chat.sendMessage(prompt);
-              const response = await result.response;
-              const text = response.text();
-              // const result = await model.generateContent(prompt);
-              // const text = result.response.text();
-              socket.emit("myEvent", text);
-              localStorage.setItem(
-                historyKey,
-                JSON.stringify(
-                  history.concat([
-                    { role: "user", parts: data },
-                    { role: "model", parts: text },
-                  ])
-                )
-              );
+              const resultNonJson = await model.generateContent(prompt);
+              const textNonJson = resultNonJson.response.text();
+
+              const promptJson =
+                textNonJson +
+                `\n \n Please provide the details of the recommended flight in the following format: \n
+              {
+                "flight_number": "ABC123",
+                "departure_time": "2024-04-07T12:00:00Z",
+                "arrival_time": "2024-04-07T15:00:00Z",
+                "departure_airport": "JFK",
+                "arrival_airport": "LAX",
+                "price": "500",
+                "airline": "Example Airlines"
+              }
+              \n\n`;
+
+              if (promptJson) {
+                const result = await chat.sendMessage(promptJson);
+                const response = await result.response;
+                const text = response.text();
+                console.log("text", text);
+                // const result = await model.generateContent(prompt);
+                // const text = result.response.text();
+                socket.emit("myEvent", text);
+                localStorage.setItem(
+                  historyKey,
+                  JSON.stringify(
+                    history.concat([
+                      { role: "user", parts: data },
+                      { role: "model", parts: text },
+                    ])
+                  )
+                );
+              }
             }
           }
         } else {
+          console.log("else statement called ");
           const gptPrompt = `Previous chat response: \n \n
             ${history}
     
